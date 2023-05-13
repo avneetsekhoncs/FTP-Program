910ee9c3a899b0d77b0c43d557b6b2a918ec9184
Avneet Sekhon <avneetsekhonfp@gmail.com>
2023-05-12 21:16:09 -0700

    Transfer file from client to server

--- client.py
+++ client.py
@@ -1,5 +1,9 @@
-import socket
+import socket   #Socket interface
+import os       #Operating system interfaces
+import tqdm
 
+SPLIT = "<SPLIT>"
+BUF_SIZE = 4096                                      #Maximum anount of data received at once
 HOST = socket.gethostbyname(socket.gethostname())       #Public network interface
 PORT = 50007                                            #Arbritary port number
 
@@ -8,3 +12,20 @@ print(f"[+] Connecting to {HOST}:{PORT}")
 
 s.connect((HOST, PORT))                                 #Connect to remote socket at address
 print("[+] Connected to ", HOST)
+
+filename = input("Select file to transfer: ")           #User input
+filesize = os.path.getsize(filename)                    #Get size of file at local path
+
+s.send(f"{filename}{SPLIT}{filesize}".encode())     #Send the file over the connection and encode it to utf-8 format (default).
+
+progress = tqdm.tqdm(range(filesize), f"Downloading {filename}", unit="B", unit_scale = True, unit_divisor=1024)        #"B" = specified units BYTES, unit_scale = use scaled units instead og just bytes KB MB GB, unit_divisor = divisor used to calculate scaled unit
+
+with open(filename, "rb") as file:                      #open file for writing in binary mode
+    while True:
+        bytes_downloaded = file.read(BUF_SIZE)          #read data of specified size
+        if bytes_downloaded == b'':                        #break if empty bytes object is returned
+            break
+        s.sendall(bytes_downloaded)
+        progress.update(len(bytes_downloaded))          #update progress bar
+
+s.close()                   #close socket
--- server.py
+++ server.py
@@ -1,7 +1,12 @@
 import socket
+import os
+import tqdm
+
 
 HOST = socket.gethostbyname(socket.gethostname())       #Public network interface
 PORT = 50007                                            #Arbritary port number
+BUF_SIZE = 4096                                      #Maximum anount of data received at once
+SPLIT = "<SPLIT>"
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)       #Address family, socket type
 s.bind((HOST, PORT))                                        #Bind the socket to the host at the specified port
@@ -10,9 +15,23 @@ s.listen(10)                                                #10 specifies the nu
 print(f"[*] Listening as {HOST}:{PORT}")
 print("Waiting for the client to connect... ")
 
-while True:
-    conn, addr = s.accept()                                     #Accept a connection
-    print(f"[+] {addr} is connected.")
+conn, addr = s.accept()                                     #Accept a connection
+print(f"[+] {addr} is connected.")
 
+r = conn.recv(BUF_SIZE).decode()                            #Translate received data
+filename, filesize = r.split(SPLIT)                         #Split received data into filename and filesize
+filename = os.path.basename(filename)
+filesize = int(filesize)
 
+progress = tqdm.tqdm(range(filesize), f"Downloading {filename}", unit="B", unit_scale = True, unit_divisor = 1024)        #"B" = specified units BYTES, unit_scale = use scaled units instead og just bytes KB MB GB, unit_divisor = divisor used to calculate scaled unit
 
+with open(filename, "wb") as file:                      #open file for writing in binary mode
+    while True:
+        bytes_downloaded = conn.recv(BUF_SIZE)          #get data of specified size
+        if bytes_downloaded == b'':                        #break if empty bytes object is returned
+            break
+        file.write(bytes_downloaded)
+        progress.update(len(bytes_downloaded))          #update progress bar
+
+conn.close()                #close connection
+s.close()                   #close socket
